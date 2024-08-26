82596c3232025dc9eaff1589117d13a1607dd863
felipemadeit <felipemadeit@gmail.com>
2024-08-25 21:02:46 -0500

    Pendiente por resolver ejercicio

---
+++ python/maxProfit.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(prices: list[int]):
+        
+        
+        
+      
+        
+solve = Solution.maxProfit([2,4,1])
+            
+        
+       
+                
+        
+        
+        

c4292e931f27769a0c631aa7bff1fa1e93782203
felipemadeit <felipemadeit@gmail.com>
2024-08-28 22:53:02 -0500

    Practice

--- python/lengthOfLastWord.py
+++ python/lengthOfLastWord.py
@@ -2,8 +2,27 @@ class Solution:
     
      def lengthOfLastWord(s: str):
          
-        for letter in s:
-            if letter != '':
+        
+        reversed_s = s[::-1].strip()
+        
+        counter = 0
+        
+        for letter in reversed_s:
+            if letter != ' ':
+                counter += 1
+            else:
+                break
+        
+        return counter
+            
+            
+                
+        
+            
+            
+
+solve = Solution.lengthOfLastWord("luffy is still joyboy")
+            
                 
              
             

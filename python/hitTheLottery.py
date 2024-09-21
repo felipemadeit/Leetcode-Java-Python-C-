5ab86e82865324d04547e7dddebffca037194a95
felipemadeit <felipemadeit@gmail.com>
2024-09-21 09:33:21 -0500

    Problema de hit the lottery resuelto en python

--- python/hitTheLottery.py
+++ python/hitTheLottery.py
@@ -2,18 +2,24 @@
 denominations = [100, 20, 10, 5, 1]
 
 n = int(input())
-
 total = 0
+index_den = 0
+rest = 0
+while index_den < len(denominations):
+    
+    if denominations[index_den] <= n:
+        total += int(n/denominations[index_den]) 
+        rest =  n -int(n / denominations[index_den])*denominations[index_den]
+        n = rest
+        index_den += 1
+    else: 
+        index_den += 1
+    
+print(total)
 
-position = 0
 
-while total < n:
-    if denominations[position] < n:
-        position += denominations[position]
-        if n % denominations[position] < denominations[position]:
-            position += 1
 
-print(total)        
+
 
 
 
